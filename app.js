926b6bd723149462673f3f05a418a53c761df7b5
Martin Polito <politomartin00@gmail.com>
2024-07-17 14:24:01 -0300

    Base de headder

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require ('express');
+const path = require ('path');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+})
